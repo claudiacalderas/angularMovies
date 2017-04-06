6251d8b4fc4f395bbe2456ebf9924b3db45859ff
Claudia Calderas <claudia.calderas@gmail.com>
2017-04-06 16:41:16 -0500

    finished required functionality

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,34 +1,32 @@
 var myApp = angular.module("myApp", []);
 
 myApp.controller("FirstController", [ "$scope", "OnlyService", function($scope, OnlyService){
-console.log("FirstController hit!");
-  $scope.allMovies = OnlyService.allMovies;
-  $scope.newMovie = {
+  $scope.newMovie = OnlyService.movieObject;
+  $scope.addMovie = OnlyService.addMovie;
+}]);
+
+myApp.controller("SecondController", ["$scope", "OnlyService", function($scope, OnlyService){
+  $scope.movies = OnlyService.allMovies.movies;
+}]);
+
+myApp.factory("OnlyService", [function(){
+
+  // this object is used to be able to clear the inputs once the movie is added
+  // data bound to inputs in the HTMl through FirstController
+  var movieObject = {
         name: "",
         desc: "",
         director: "",
         length: ""
       };
-  $scope.addMovie = OnlyService.addMovie;
 
-}]);
-
-myApp.controller("SecondController", ["$scope", "OnlyService", function($scope, OnlyService){
-  console.log("SecondController hit!");
-  $scope.allMovies = OnlyService.allMovies;
-  $scope.movies = OnlyService.allMovies.movies;
-
-}]);
-
-
-
-myApp.factory("OnlyService", [function(){
-console.log("OnlyService hit!");
-
+  // Object that contains the array of movies
   var allMovies = {
         movies: []
       };
 
+  // Function that adds a new movie to the array. Called from the button in the
+  // div controlled by FirstController
   var addMovie = function(name, desc, director, length){
     var movie = {
       name: name,
@@ -38,11 +36,17 @@ console.log("OnlyService hit!");
     };
     allMovies.movies.push(movie);
     console.log(allMovies.movies);
+    // This is how we reinitialized the inputs:
+    movieObject.name = "";
+    movieObject.desc = "";
+    movieObject.director = "";
+    movieObject.length = "";
   };
 
   return {
     allMovies: allMovies,
     addMovie: addMovie,
+    movieObject: movieObject
   };
 
 }]);
